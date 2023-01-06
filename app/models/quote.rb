7b1b6ea0ab489bcb87614469fe7acf152a70cac2
Kedaruma-Bond <brianbondwhippet@gmail.com>
2023-01-06 12:07:01 +0900

    upto chap6 これ本来はchap0から始まるからchap5が終わったとこやね

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,6 +2,9 @@ class Quote < ApplicationRecord
   validates :name, presence: true
   
   scope :ordered, -> { order(id: :desc) }
-  after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
-  after_update_commit -> { broadcast_replace_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+  after_create_commit -> { broadcast_prepend_later_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+  after_update_commit -> { broadcast_replace_later_to "quotes", partial: "quotes/quote", locals: { quote: self } }
+  after_destroy_commit -> { broadcast_remove_to "quotes" }
+  # Those three callbacks are equivalent to the following single line でもわかりにくいですよね…
+  # broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
 end
