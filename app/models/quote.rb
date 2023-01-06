7f2f9d803fda173504aa87552f37eb06e9639eae
Kedaruma-Bond <brianbondwhippet@gmail.com>
2023-01-06 18:33:39 +0900

    upto chap7-1

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,10 +1,12 @@
 class Quote < ApplicationRecord
+  belongs_to :company
+
   validates :name, presence: true
   
   scope :ordered, -> { order(id: :desc) }
-  after_create_commit -> { broadcast_prepend_later_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
-  after_update_commit -> { broadcast_replace_later_to "quotes", partial: "quotes/quote", locals: { quote: self } }
-  after_destroy_commit -> { broadcast_remove_to "quotes" }
-  # Those three callbacks are equivalent to the following single line でもわかりにくいですよね…
-  # broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+  after_create_commit -> { broadcast_prepend_later_to [company, "quotes"], partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
+  after_update_commit -> { broadcast_replace_later_to [company, "quotes"], partial: "quotes/quote", locals: { quote: self } }
+  after_destroy_commit -> { broadcast_remove_to [company, "quotes"] }
+  # Those three callbacks are equivalent to the following single line
+  # broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
 end
